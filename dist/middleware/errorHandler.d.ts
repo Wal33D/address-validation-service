89d532e2920c1e238fc5397ae52ac84114df8cb1
Wal33D <aquataze@yahoo.com>
2025-07-30 06:56:08 -0400

    Untrack dist directory from git

--- dist/middleware/errorHandler.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction): void;
-export declare function asyncHandler(fn: Function): (req: Request, res: Response, next: NextFunction) => void;
-export declare function notFoundHandler(_req: Request, res: Response): void;
-//# sourceMappingURL=errorHandler.d.ts.map
--- dist/middleware/localOnlyMiddleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare function localOnlyMiddleware(req: Request, res: Response, next: NextFunction): void;
-//# sourceMappingURL=localOnlyMiddleware.d.ts.map
